16fa4785d42f488b09d294aa802b485080a7c485
Alux <plutocoding@gmail.com>
2023-10-25 23:06:27 -0400

    generating the readme, using object methods, string interpolation

--- Develop/index.js
+++ Develop/index.js
@@ -1,6 +1,17 @@
 // const
+const fs = require(`fs`);
 const inquirer = require(`inquirer`);
 const generateMarkdown = require(`./utils/generateMarkdown`);
+
+const licenses = {
+    none: `None`,
+    MIT: `MIT // Massachusetts Institute of Technology License`,
+    BSD: `BSD // Berkeley Software Distribution License`,
+    ISC: `ISC // Internet Software Consortium License`,
+    GPL: `GPL // GNU General Public License`,
+    APACHE: `APACHE // Apache License`,
+}
+
 // const futureConstHere
 const rejexEmailValidation = (value) => {
     let rejexEmailValidationInner = value.match(
@@ -41,11 +52,8 @@ const questions = [
         message: "Enter your application/repo description.",
         default: `New Application`,
     }, 
-    // installation instructions
     // Does this app need testing
     // is this a team project?
-    // what is the purpose/usage?
-    // is there a preview link or image?
     {
         type: `input`,
         name: `email`,
@@ -63,39 +71,75 @@ const questions = [
     {
         type: `input`,
         name: `username`,
-        message: `Enter your Github Username.` 
+        message: `Enter your Github Username.`, 
+    },
+    {
+        type: `confirm`,
+        name: `ifPreview`,
+        message: `Do you have a valid link to either an Image, GIF, or Video of the Application?`,
+        default: false,
+    },
+    {
+        type: `input`,
+        name: `preview`,
+        message: `Please enter the link of the image, GIF, or Video of the Application.`,
+        // When is a method of Inquirer, conditionally adds questions depending on what the user said previously
+        when(response) {
+            return response.ifPreview == true;
+        },
+    },
+    {
+        name: `installation`,
+        type: `input`,
+        message: `What are the Application Installation Instructions?`,
+        default: `NPM Install`
+    },
+    {
+        name: `usage`,
+        type: `input`,
+        message: `What are the Directions for Usage?`,
+        default: `Node.js`
     },
     {
         type: `list`,
         name: `license`,
         message: `What license does this application utilize?`,
         choices: [
-            `none`,
-            `MIT // Massachusetts Institute of Technology License`,
-            `BSD // Berkeley Software Distribution License`,
-            `ISC // Internet Software Consortium License`,
-           `GPL // GNU General Public License`,
-           `APACHE // Apache License`,
+            licenses.none,
+            licenses.MIT,
+            licenses.BSD,
+            licenses.ISC,
+            licenses.GPL,
+            licenses.APACHE,
         ],
         default: 1,
     },
 ];
 
+// TODO: Create a function to write README file
+// you can not do fs without node
+// fs stands for file system (file system library)
+// fs has methods, such as writeFile, read, etc
+const writeToFile = (fileName, template) => {
+    fs.writeFile(fileName, template, (error => {
+            error ? console.log(error) : console.log(`README Successfully Generated! You can find it within this folder!`);
+        })
+    )
+};
+
 const askQuestions = () => {
     inquirer.prompt(questions).then((answers) => {
         console.log(`answers to questions:`, answers);
+        // use the answers object to inject variables into the readme template which we will generate
+        let generatedMarkdownTemplate = generateMarkdown(answers);
+        writeToFile(`GeneratedREADME.md`, generatedMarkdownTemplate);
     }).catch((error) => {
         if (error.isTtyError) {
-          // Prompt couldn't be rendered in the current environment
+          console.log(`Prompt couldn't be rendered in the current environment`);
         } else {
-          // Something else went wrong
+          console.log(`Something else went wrong`);
         }
     });
-};
-
-// TODO: Create a function to write README file
-const writeToFile = (fileName, data) => {
-
 };
 
 // TODO: Create a function to initialize app
