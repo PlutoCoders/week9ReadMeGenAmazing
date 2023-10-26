13fc75b559cb9bc7e4ba3648afc8fe18bc7c9cd8
Alux <plutocoding@gmail.com>
2023-10-25 20:20:15 -0400

    email validation hooked up

--- Develop/index.js
+++ Develop/index.js
@@ -2,6 +2,29 @@
 const inquirer = require(`inquirer`);
 const generateMarkdown = require(`./utils/generateMarkdown`);
 // const futureConstHere
+const rejexEmailValidation = (value) => {
+    let rejexEmailValidationInner = value.match(
+        /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|.(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+    );
+    return rejexEmailValidationInner;
+};
+
+const vanillaJavaScriptValidation = (value) => {
+    const validDomains = [`com`, `org`, `gov`, `edu`];
+    const errorString = "Please enter a valid email!";
+    // includes returns true/false
+    let emailHasAt = value.includes(`@`);
+    if (emailHasAt == true) {
+        // split returns an array
+        let emailWebsite = value.split(`@`)[1];
+        let emailDomain = emailWebsite.split(`.`)[1];
+        let emailHasValidDomain = validDomains.includes(emailDomain);
+        // all of these have to be true for the email to be valid
+        return value != `` && emailHasValidDomain == true ? true : errorString;
+    } else {
+        return errorString;
+    }
+};
 
 // TODO: Include packages needed for this application
 // TODO: Create an array of questions for user input
@@ -17,15 +40,30 @@ const questions = [
         name: 'description',
         message: "Enter your application/repo description.",
         default: `New Application`,
-        // validate(value) {
-        //     const pass = value.match(
-        //     /^([01]{1})?[-.\s]?\(?(\d{3})\)?[-.\s]?(\d{3})[-.\s]?(\d{4})\s?((?:#|ext\.?\s?|x\.?\s?){1}(?:\d+)?)?$/i,
-        //     );
-        //     if (pass) {
-        //         return true;
-        //     }
-        //     return 'Please enter a valid phone number';
-        // },
+    }, 
+    // installation instructions
+    // Does this app need testing
+    // is this a team project?
+    // what is the purpose/usage?
+    // is there a preview link or image?
+    {
+        type: `input`,
+        name: `email`,
+        message: `What is the email linked to your github account?`,
+        //even if someone types it all in capital, it will be set to the standard format
+        filter(value) {
+            return value.toLowerCase();
+        },
+        //validate comes from inquirer, and gives us a free variable called value
+        validate(value) {
+         return vanillaJavaScriptValidation(value);
+        //  return vanillaJavaScriptValidation(value) && rejexEmailValidation(value);
+        },  
+    },
+    {
+        type: `input`,
+        name: `username`,
+        message: `Enter your Github Username.` 
     },
     {
         type: `list`,
@@ -40,7 +78,7 @@ const questions = [
            `APACHE // Apache License`,
         ],
         default: 1,
-    }
+    },
 ];
 
 const askQuestions = () => {
